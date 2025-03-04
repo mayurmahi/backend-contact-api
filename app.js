aae4aeefd7ef9ba3db5ae9019ed5309021caa7c7
mayurmahi <mayurmahindrakar444@gmail.com>
2025-03-04 18:55:20 +0530

    cors enabling

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const userRoute = require('./routes/user')
 const contactRoute = require('./routes/contact')
 
+// Enable CORS
+app.use(cors())
 
-mongoose.connect('mongodb+srv://sbs:sbs123@sbs.98ss74j.mongodb.net/?retryWrites=true&w=majority&appName=SBS')
-.then(res=>{console.log("connected to database")})
-.catch(err=>{console.log(err)})
+mongoose.connect('mongodb+srv://mayur:mayur@cluster0.7dydx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(res => { console.log("Connected to database") })
+.catch(err => { console.log(err) })
 
 app.use(bodyParser.json())
 
-app.use('/user',userRoute)
-app.use('/contact',contactRoute)
+app.use('/user', userRoute)
+app.use('/contact', contactRoute)
 
-
-app.use('*',(req,res)=>{
-    res.status(404).json({
-        msg:'bad request'
-    })
+// Handle 404
+app.use('*', (req, res) => {
+    res.status(404).json({ msg: 'Bad request' })
 })
 
 module.exports = app
